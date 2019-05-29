76f058ba8e84f861941680518731ae565436cced
Lucas Ursulino Boaventura <lucasxboaventura@hotmail.com>
2019-05-29 13:26:49 -0300

    Adicionar Soluções em C para os alunos de APC

--- Lista4/monitor04017.c
+++ Lista4/monitor04017.c
@@ -1,62 +1,40 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
 
     int array[8][8], SD = 0, SL, somaL[8], i, j, n;
     int resto[8], restomaior = 0;
+
     //MONTAGEM DO ARRAY
-
-    for (i = 0; i < 8; i++)
-    {
-
-        for (j = 0; j < 8; j++)
-        {
-
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
             scanf("%d", &array[i][j]);
         }
     }
 
     //SOMA DA DIAGONAL PRINCIPAL
-
-    for (i = 0; i < 8; i++)
-    {
-
+    for (i = 0; i < 8; i++) {
         j = i;
-
         SD += array[i][j];
     }
 
     //SOMA DE CADA LINHA
-
-    for (i = 0; i < 8; i++)
-    {
-
+    for (i = 0; i < 8; i++) {
         SL = 0;
-
-        for (j = 0; j < 8; j++)
-        {
-
+        for (j = 0; j < 8; j++) {
             SL += array[i][j];
         }
         somaL[i] = SL;
     }
 
     //Calculo do Resto
-    for (i = 0; i < 8; i++)
-    {
-
+    for (i = 0; i < 8; i++) {
         resto[i] = somaL[i] % SD;
     }
 
     //Comparações
-
-    for (i = 0; i < 8; i++)
-    {
-
-        if (resto[i] >= restomaior)
-        {
-
+    for (i = 0; i < 8; i++) {
+        if (resto[i] >= restomaior) {
             restomaior = resto[i];
             n = i;
         }
@@ -67,7 +45,7 @@ int main()
     return 0;
 }
 
-// 83 89 91 60 20 83 40 75 
+// 83 89 91 60 20 83 40 75
 // 12 14 36 43 43 11 90 33
 // 15 74 90 79 40 85 77 63
 // 74 69 38 49 76 29 13 43
